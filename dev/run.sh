b3d81b3772d22b31373de7ae162dcc505bc14888
Björn Nordqvist <bjorn@nordqvist.io>
2018-02-07 17:18:40 +0100

    Fixes

--- dev/run.sh
+++ dev/run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+echo "Setup required folders and permissions!"
+mkdir -p \
+  /app/storage/ \
+  /app/storage/framework/sessions \
+  /app/storage/framework/views \
+  /app/storage/framework/cache \
+  /app/storage/app \
+  /app/storage/logs \
+  /app/storage/uploads
+chown -R www-data:www-data /app
+chmod -R 775 /app/storage
 echo "Cleaning up!"
 apt-get clean
 rm -r /var/lib/apt/lists/*
+php /app/artisan optimize
+php /app/artisan config:cache
+php /app/artisan route:cache
 echo "Starting Apache!"
 apache2-foreground
